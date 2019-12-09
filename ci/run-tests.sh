cf4ebbebc24a629d49e154d4efc5dcc84fb10550
Jacek Olszak <jacekolszak@gmail.com>
2019-12-09 21:31:45 +0100

    Add dockerfile for CI

---
+++ ci/run-tests.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+# Run virtual frame buffer with Display :99
+Xvfb :99 -ac -screen 0 "1920x1080x24" -nolisten tcp &
+
+# TODO go test -race does not work on Alpine :(
+go test -v ./... | tee tests.log
