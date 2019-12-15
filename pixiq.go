9e1dde301369eb573b4c44c4919200037b298a8f
Jacek Olszak <jacekolszak@gmail.com>
2019-12-15 17:31:16 +0100

    Add selection (#4)
    * Add more tests for images.New
    
    * Add Selection
    
    * Add documentation
    
    * use struct literal
    
    * use 0 size image

--- pixiq.go
+++ pixiq.go
@@ -39,3 +39,52 @@ func (i *Image) Width() int {
 func (i *Image) Height() int {
 	return i.height
 }
+
+// Selection makes a rectangular selection starting at a given position. The position has to be top-left position.
+// Both x and y can be negative, meaning that selection starts outside the image.
+func (i *Image) Selection(x int, y int) Selection {
+	return Selection{
+		x: x,
+		y: y,
+	}
+}
+
+// Selection marks a selection on top of the image.
+type Selection struct {
+	x, y, width, height int
+}
+
+// Width returns the width of selection in pixels.
+func (s Selection) Width() int {
+	return s.width
+}
+
+// Height returns the height of selection in pixels.
+func (s Selection) Height() int {
+	return s.height
+}
+
+// X returns the starting position
+func (s Selection) X() int {
+	return s.x
+}
+
+// Y returns the starting position
+func (s Selection) Y() int {
+	return s.y
+}
+
+// WithSize creates a new selection with specified size in pixels. Width or height are clamped to 0
+func (s Selection) WithSize(width, height int) Selection {
+	if width > 0 {
+		s.width = width
+	} else {
+		s.width = 0
+	}
+	if height > 0 {
+		s.height = height
+	} else {
+		s.height = 0
+	}
+	return s
+}
--- pixiq_test.go
+++ pixiq_test.go
@@ -29,11 +29,110 @@ func TestImages_New(t *testing.T) {
 		require.NotNil(t, image)
 		assert.Equal(t, 0, image.Height())
 	})
-	t.Run("should create a new image", func(t *testing.T) {
+	t.Run("should create an image of any size", func(t *testing.T) {
+		// given
+		tests := map[string]struct {
+			width, height int
+		}{
+			"0x0": {
+				width:  0,
+				height: 0,
+			},
+			"0x1": {
+				width:  0,
+				height: 1,
+			},
+			"1x0": {
+				width:  1,
+				height: 0,
+			},
+			"1x1": {
+				width:  1,
+				height: 1,
+			},
+			"2x3": {
+				width:  2,
+				height: 3,
+			},
+		}
 		images := pixiq.NewImages()
-		image := images.New(2, 3)
-		require.NotNil(t, image)
-		assert.Equal(t, 2, image.Width())
-		assert.Equal(t, 3, image.Height())
+		for name, test := range tests {
+			t.Run(name, func(t *testing.T) {
+				// when
+				image := images.New(test.width, test.height)
+				// then
+				require.NotNil(t, image)
+				assert.Equal(t, test.width, image.Width())
+				assert.Equal(t, test.height, image.Height())
+			})
+		}
+	})
+}
+
+func TestImage_Selection(t *testing.T) {
+	images := pixiq.NewImages()
+	image := images.New(4, 2)
+
+	t.Run("should create a selection for negative x", func(t *testing.T) {
+		selection := image.Selection(-1, 0)
+		require.NotNil(t, selection)
+		assert.Equal(t, selection.X(), -1)
+	})
+
+	t.Run("should create a selection for negative y", func(t *testing.T) {
+		selection := image.Selection(0, -1)
+		require.NotNil(t, selection)
+		assert.Equal(t, selection.Y(), -1)
+	})
+
+	t.Run("should create a selection", func(t *testing.T) {
+		selection := image.Selection(1, 2)
+		require.NotNil(t, selection)
+		assert.Equal(t, selection.X(), 1)
+		assert.Equal(t, selection.Y(), 2)
+		assert.Equal(t, selection.Width(), 0)
+		assert.Equal(t, selection.Height(), 0)
+	})
+
+}
+
+func TestSelection_WithSize(t *testing.T) {
+	images := pixiq.NewImages()
+	image := images.New(0, 0)
+
+	t.Run("should set selection width to zero if given width is negative", func(t *testing.T) {
+		selection := image.Selection(1, 2)
+		// when
+		selection = selection.WithSize(-1, 4)
+		assert.Equal(t, selection.Width(), 0)
+	})
+
+	t.Run("should clamp width to zero if given width is negative and previously width was set to positive number", func(t *testing.T) {
+		selection := image.Selection(1, 2).WithSize(5, 0)
+		// when
+		selection = selection.WithSize(-1, 4)
+		assert.Equal(t, selection.Width(), 0)
+	})
+
+	t.Run("should set selection height to zero if given height is negative", func(t *testing.T) {
+		selection := image.Selection(1, 2)
+		// when
+		selection = selection.WithSize(3, -1)
+		assert.Equal(t, selection.Height(), 0)
+	})
+
+	t.Run("should clamp height to zero if given height is negative and previously height was set to positive number", func(t *testing.T) {
+		selection := image.Selection(1, 2).WithSize(0, 5)
+		// when
+		selection = selection.WithSize(3, -1)
+		assert.Equal(t, selection.Height(), 0)
+	})
+
+	t.Run("should set selection size", func(t *testing.T) {
+		selection := image.Selection(1, 2)
+		// when
+		selection = selection.WithSize(3, 4)
+		assert.Equal(t, selection.Width(), 3)
+		assert.Equal(t, selection.Height(), 4)
 	})
 }
