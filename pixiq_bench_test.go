5b536b05d08a24bce6ff9e89d1b6cc66f41e6114
Jacek Olszak <jacekolszak@gmail.com>
2019-12-20 20:24:17 +0100

    Add benchmark

---
+++ pixiq_bench_test.go
@@ -0,0 +1,42 @@
+package pixiq_test
+
+import (
+	"testing"
+
+	"github.com/jacekolszak/pixiq"
+)
+
+func BenchmarkSelection_SetColor(b *testing.B) {
+	b.StopTimer()
+	color := pixiq.RGBA(10, 20, 30, 40)
+	images := pixiq.NewImages()
+	image := images.New(1920, 1080)
+	selection := image.WholeImageSelection()
+	height := selection.Height()
+	width := selection.Width()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for y := 0; y < height; y++ {
+			for x := 0; x < width; x++ {
+				selection.SetColor(x, y, color)
+			}
+		}
+	}
+}
+
+func BenchmarkSelection_Color(b *testing.B) {
+	b.StopTimer()
+	images := pixiq.NewImages()
+	image := images.New(1920, 1080)
+	selection := image.WholeImageSelection()
+	height := selection.Height()
+	width := selection.Width()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for y := 0; y < height; y++ {
+			for x := 0; x < width; x++ {
+				selection.Color(x, y)
+			}
+		}
+	}
+}
