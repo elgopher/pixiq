8ac66c4dde32d7c9c41aba796bd2098c90966bf1
Jacek Olszak <jacekolszak@gmail.com>
2019-12-25 14:00:02 +0100

    add gl placeholders

--- opengl/opengl.go
+++ opengl/opengl.go
@@ -4,13 +4,20 @@ import "github.com/jacekolszak/pixiq"
 
 // Run should be executed in main thread.
 func Run(runInDifferentGoroutine func(images *pixiq.Images)) {
-	images := pixiq.NewImages(&glTextures{})
+	images := pixiq.NewImages(&textures{})
 	runInDifferentGoroutine(images)
 }
 
-type glTextures struct {
+type textures struct {
 }
 
-func (g *glTextures) New(width, height int) pixiq.AcceleratedImage {
+func (g *textures) New(width, height int) pixiq.AcceleratedImage {
+	panic("implement me")
+}
+
+type texture struct {
+}
+
+func (t *texture) Upload(pixels []pixiq.Color) {
 	panic("implement me")
 }
