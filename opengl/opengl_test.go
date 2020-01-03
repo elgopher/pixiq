98f30a7c9a0d6bbbf756cccf5b4fc4691a304c0e
Jacek Olszak <jacekolszak@gmail.com>
2020-01-03 21:46:05 +0100

    Clamp window size to platform-specific minimums

--- opengl/opengl_test.go
+++ opengl/opengl_test.go
@@ -95,30 +95,30 @@ func TestTexture_Upload(t *testing.T) {
 }
 
 func TestGlfwWindows_Open(t *testing.T) {
-	t.Run("should clamp width to 1 if negative", func(t *testing.T) {
+	t.Run("should clamp width to platform-specific minimum if negative", func(t *testing.T) {
 		openGL := opengl.New(mainThreadLoop)
 		windows := openGL.Windows()
 		// when
 		win := windows.Open(-1, 0)
 		require.NotNil(t, win)
-		assert.Equal(t, 1, win.Width())
+		assert.GreaterOrEqual(t, win.Width(), 0)
 	})
-	t.Run("should clamp height to 1 if negative", func(t *testing.T) {
+	t.Run("should clamp height to platform-specific minimum if negative", func(t *testing.T) {
 		openGL := opengl.New(mainThreadLoop)
 		windows := openGL.Windows()
 		// when
 		win := windows.Open(0, -1)
 		require.NotNil(t, win)
-		assert.Equal(t, 1, win.Height())
+		assert.GreaterOrEqual(t, win.Height(), 0)
 	})
 	t.Run("should open Window", func(t *testing.T) {
 		openGL := opengl.New(mainThreadLoop)
 		windows := openGL.Windows()
 		// when
-		win := windows.Open(1, 2)
+		win := windows.Open(640, 360)
 		require.NotNil(t, win)
-		assert.Equal(t, 1, win.Width())
-		assert.Equal(t, 2, win.Height())
+		assert.Equal(t, 640, win.Width())
+		assert.Equal(t, 360, win.Height())
 	})
 }
 
