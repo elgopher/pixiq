b43cdd4a8e149cb665e284b3f9a852451874304e
Jacek Olszak <jacekolszak@gmail.com>
2020-01-05 19:41:11 +0100

    Fix typo

--- cmd/examples/windows/closing/main.go
+++ cmd/examples/windows/closing/main.go
@@ -10,7 +10,7 @@ func main() {
 	opengl.Run(func(gl *opengl.OpenGL, images *pixiq.Images, loops *pixiq.ScreenLoops) {
 		window := gl.Windows().Open(320, 180)
 		loops.Loop(window, func(frame *pixiq.Frame) {
-			// If user was closed by the user ShouldClose will return true
+			// If window was closed by the user ShouldClose will return true
 			if window.ShouldClose() {
 				// Stop the loop at the end of the iteration
 				frame.StopLoopEventually()
