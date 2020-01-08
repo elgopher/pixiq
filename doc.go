3325b45bd05060149a615e170d857be76542c344
Jacek Olszak <jacekolszak@gmail.com>
2020-01-08 21:34:11 +0100

    Add dummy doc

--- doc.go
+++ doc.go
@@ -1,4 +1,16 @@
-/*
-Package pixiq allows you to manipulate images in real-time using Pixel-Perfect API.
-*/
+// Package pixiq allows you to manipulate images in real-time using Pixel-Perfect API.
+//
+// Before Image can be created a factory should be created:
+//
+//	   images := pixiq.NewImages(acceleratedImages) // i.e. gl.AcceleratedImages()
+//
+// Then multiple images can be created:
+//
+//	   image := images.New(2, 2)
+//
+// And pixel set using Selection:
+//
+//     wholeSelection := image.WholeImageSelection()
+// 	   wholeSelection.SetColor(0, 1, colornames.White)
+//
 package pixiq
