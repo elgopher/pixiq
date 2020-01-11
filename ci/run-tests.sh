7040a1acba611968617a0ad3974b07fed1a06b61
Jacek Olszak <jacekolszak@gmail.com>
2020-01-11 11:32:50 +0100

    Removing. Found problem somewhere else

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -4,6 +4,3 @@
 Xvfb :99 -ac -screen 0 "1920x1080x24" -nolisten tcp &
 
 go test -race -v ./...
-go test -race -run "TestWindows_Open" -v ./opengl
-go test -race -run "TestWindow_Zoom" -v ./opengl
-go test -race -v ./opengl/internal
