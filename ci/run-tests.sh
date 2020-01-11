0ab823c8d50e487ce3377a8d7f2000eebac1ffaa
Jacek Olszak <jacekolszak@gmail.com>
2020-01-11 11:13:40 +0100

    Temporrary commands

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -4,5 +4,6 @@
 Xvfb :99 -ac -screen 0 "1920x1080x24" -nolisten tcp &
 
 go test -race -v ./...
-go test opengl -v
-go test opengl -v -run "TestRun"
+go test -v ./opengl
+go test -run "TestRun" -v ./opengl
+go test -run "TestWindow_Draw" -v ./opengl
