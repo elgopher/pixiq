79b61f5aba41c299fcdc2ab0d7a7b27a18118d10
Jacek Olszak <jacekolszak@gmail.com>
2020-01-11 11:19:44 +0100

    Add -race

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -4,5 +4,5 @@
 Xvfb :99 -ac -screen 0 "1920x1080x24" -nolisten tcp &
 
 go test -race -v ./...
-go test -run "TestWindows_Open" -v ./opengl
-go test -run "TestWindow_Zoom" -v ./opengl
+go test -race -run "TestWindows_Open" -v ./opengl
+go test -race -run "TestWindow_Zoom" -v ./opengl
