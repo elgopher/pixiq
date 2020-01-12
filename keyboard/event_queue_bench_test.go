464e13e6f8b4b3e17ae8a05a49103aac626ddf5a
Jacek Olszak <jacekolszak@gmail.com>
2020-01-12 15:46:57 +0100

    Overflow event queue in benchmark

--- keyboard/event_queue_bench_test.go
+++ keyboard/event_queue_bench_test.go
@@ -13,7 +13,7 @@ func BenchmarkKeyboardEvents(b *testing.B) {
 	events := keyboard.NewEventQueue(size)
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		for i := 0; i < size; i++ {
+		for i := 0; i < size*2; i++ {
 			events.Append(keyboard.NewPressedEvent(keyboard.A))
 		}
 		for {
