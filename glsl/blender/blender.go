bbb88bf2da00b952407b4eab4dc537b80c58f932
Jacek Olszak <jacekolszak@gmail.com>
2020-01-19 13:16:06 +0100

    Add info that we should use a global Blender interface.

--- glsl/blender/blender.go
+++ glsl/blender/blender.go
@@ -9,9 +9,20 @@ func CompileImageBlender(compiler shader.Compiler) (*ImageBlender, error) {
 	if compiler == nil {
 		panic("nil compiler")
 	}
+	// TODO Here we can verify that blending really work on this platform
+	// by executing blend. If it does not we can return error. This is much
+	// better than panicking during real Blending
 	return &ImageBlender{}, nil
 }
 
+// TODO Refactor this a bit - it should stick to the global interface
+// for all sorts of blending to support Go implicit interfaces:
+//
+//   type Blender interface {
+//	   Blend(source, target image.Selection)
+//   }
+//
+// Eventually blend function can be run using Blend(source,target)
 type ImageBlender struct {
 }
 
