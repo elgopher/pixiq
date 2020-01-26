123c004b72f707ae2f8fe253d02a19aadaea5a79
Jacek Olszak <jacekolszak@gmail.com>
2020-01-26 13:45:28 +0100

    Add docs

--- image/fake/fake.go
+++ image/fake/fake.go
@@ -6,19 +6,21 @@ import (
 	"github.com/jacekolszak/pixiq/image"
 )
 
-// NewAccelerator returns a new container of accelerated images and programs.
-// It can be used in unit tests as a replacement for a real implementation
-// (such as OpenGL).
+// NewAccelerator returns a new instance of Accelerator.
 func NewAccelerator() *Accelerator {
 	return &Accelerator{
 		programs: map[image.AcceleratedProgram]*program{},
 	}
 }
 
+// Accelerator is a container of accelerated images and programs.
+// It can be used in unit tests as a replacement for a real implementation
+// (such as OpenGL).
 type Accelerator struct {
 	programs map[image.AcceleratedProgram]*program
 }
 
+// NewImage returns a new instance of *AcceleratedImage
 func (i *Accelerator) NewImage(imageWidth, imageHeight int) *AcceleratedImage {
 	return &AcceleratedImage{
 		programs:    i.programs,
@@ -27,16 +29,18 @@ func (i *Accelerator) NewImage(imageWidth, imageHeight int) *AcceleratedImage {
 	}
 }
 
+// NewProgram returns a new instance of program which can be used to create
+// a Drawer.
 func (i *Accelerator) NewProgram(f func(img *AcceleratedImage, selection image.AcceleratedImageSelection)) image.AcceleratedProgram {
 	program := &program{f: f}
 	i.programs[program] = program
 	return program
 }
 
-// TODO Test
 // NewAddColorProgram creates a new program adding all color components to each
 // pixel in a selection.
 func (i *Accelerator) NewAddColorProgram(colorToAdd image.Color) image.AcceleratedProgram {
+	// TODO Test
 	return i.NewProgram(func(img *AcceleratedImage, selection image.AcceleratedImageSelection) {
 		for y := selection.Y; y < selection.Y+selection.Height; y++ {
 			for x := selection.X; x < selection.X+selection.Width; x++ {
@@ -65,6 +69,8 @@ type AcceleratedImage struct {
 type drawer struct {
 }
 
+// Drawer returns an AcceleratedDrawer for the program created by the same Accelerator
+// as this image.
 func (i *AcceleratedImage) Drawer(program image.AcceleratedProgram, selection image.AcceleratedImageSelection) (image.AcceleratedDrawer, error) {
 	prg, ok := i.programs[program]
 	if !ok {
@@ -74,12 +80,14 @@ func (i *AcceleratedImage) Drawer(program image.AcceleratedProgram, selection im
 	return &drawer{}, nil
 }
 
+// Upload send pixels to a container in RAM
 func (i *AcceleratedImage) Upload(pixels []image.Color) {
 	i.pixels = make([]image.Color, len(pixels))
 	// copy pixels to ensure that Upload method has been called
 	copy(i.pixels, pixels)
 }
 
+// Download fills output slice with image colors
 func (i *AcceleratedImage) Download(output []image.Color) {
 	for j := 0; j < len(output); j++ {
 		output[j] = i.pixels[j]
