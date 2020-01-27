aa7c536cd5065db0b4d086daef271c3d0f6d6ff3
Jacek Olszak <jacekolszak@gmail.com>
2020-01-27 23:09:05 +0100

    Add benchmarks

--- image/image_bench_test.go
+++ image/image_bench_test.go
@@ -42,3 +42,36 @@ func BenchmarkSelection_Color(b *testing.B) {
 		}
 	}
 }
+
+// Must be 0 allocs/op
+func BenchmarkSelection(b *testing.B) {
+	b.StopTimer()
+	var (
+		img, _    = image.New(1, 1, acceleratedImageStub{})
+		selection = img.WholeImageSelection()
+	)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		selection.Selection(0, 0).WithSize(1, 1)
+	}
+}
+
+// Should be 0 allocs/op
+func BenchmarkSelection_Modify(b *testing.B) {
+	b.StopTimer()
+	var (
+		img, _    = image.New(1920, 1080, acceleratedImageStub{})
+		selection = img.WholeImageSelection()
+		program   = struct{}{}
+		primitive = struct{}{}
+		param     = struct{}{}
+	)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		procedure := func(drawer image.Drawer) {
+			drawer.SetSelection("selection", selection)
+			_ = drawer.Draw(primitive, param)
+		}
+		_ = selection.Modify(program, procedure)
+	}
+}
