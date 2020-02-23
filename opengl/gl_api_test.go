23604da7fbbbbca562b38c84098f5dceb18548d5
Jacek Olszak <jacekolszak@gmail.com>
2020-02-23 21:14:17 +0100

    Add gl.Clear test

---
+++ opengl/gl_api_test.go
@@ -0,0 +1,21 @@
+package opengl_test
+
+import (
+	"testing"
+
+	"github.com/jacekolszak/pixiq/opengl"
+)
+
+// Must be 0 allocs/op
+func BenchmarkContext_Clear(b *testing.B) {
+	openGL, err := opengl.New(mainThreadLoop)
+	if err != nil {
+		panic(err)
+	}
+	defer openGL.Destroy()
+	gl := openGL.Context().API()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		gl.Clear(0x4000)
+	}
+}
