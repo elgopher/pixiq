5127181ccf18c491a7d7f13b96464310be0f94d7
Jacek Olszak <jacekolszak@gmail.com>
2020-03-15 22:29:07 +0100

    Rename Tool to OverrideTool

--- tools/blend/blend.go
+++ tools/blend/blend.go
@@ -2,16 +2,19 @@ package blend
 
 import "github.com/jacekolszak/pixiq/image"
 
-// Override returns new instance of *blend.Tool which will override target with source
+// Override returns new instance of *blend.OverrideTool which will override target with source
 // color
-func Override() *Tool {
-	return &Tool{}
+func Override() *OverrideTool {
+	return &OverrideTool{}
 }
 
-type Tool struct {
+type OverrideTool struct {
 }
 
-func (t *Tool) BlendSource(source, target image.Selection) {
+// BlendSource blends source into target.
+//
+// If target has 0x0 size then whole source is blended, otherwise source is clamped.
+func (t *OverrideTool) BlendSource(source, target image.Selection) {
 	lines := source.Lines()
 	for y := 0; y < lines.Length(); y++ {
 		line := lines.LineForRead(y)
