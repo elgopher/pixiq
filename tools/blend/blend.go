3246d6d76d2779ccd3062480917aa5dce9f79ef7
Jacek Olszak <jacekolszak@gmail.com>
2020-04-06 16:28:41 +0200

    add comments

--- tools/blend/blend.go
+++ tools/blend/blend.go
@@ -4,10 +4,13 @@ import (
 	"github.com/jacekolszak/pixiq/image"
 )
 
+// ColorBlender blends source and target colors together. It is executed by Tool
+// for each pixel in source and target selection.
 type ColorBlender interface {
 	BlendSourceToTargetColor(source, target image.Color) image.Color
 }
 
+// New creates a blending Tool with given ColorBlender implementation.
 func New(colorBlender ColorBlender) *Tool {
 	if colorBlender == nil {
 		panic("nil colorBlender")
@@ -17,12 +20,18 @@ func New(colorBlender ColorBlender) *Tool {
 	}
 }
 
+// NewSource creates a new blending tool which replaces target selection with source
+// colors. It is like coping of source selection colors into target.
 func NewSource() *Source {
 	return &Source{}
 }
 
+// Source is a blending tool which replaces target selection with source
+// colors. It is like coping of source selection colors into target.
 type Source struct{}
 
+// BlendSourceToTarget blends source into target. Only position of the target Selection
+// is used and the source is not clamped by the target size.
 func (s *Source) BlendSourceToTarget(source, target image.Selection) {
 	target = target.WithSize(source.Width(), source.Height())
 	var (
@@ -64,30 +73,40 @@ func (s *Source) BlendSourceToTarget(source, target image.Selection) {
 	}
 }
 
+// NewSourceOver creates a new blending tool which blends together source and target
+// taking into account alpha channel of both.
 func NewSourceOver() *SourceOver {
 	return NewSourceOverWithOpacity(100)
 }
 
+// NewSourceOver creates a new blending tool which blends together source and target
+// taking into account alpha channel of both plus the additional opacity of the source.
 func NewSourceOverWithOpacity(opacity int) *SourceOver {
 	tool := &SourceOver{opacity: opacity}
 	tool.Tool = New(tool)
 	return tool
 }
 
-// Aka Normal
+// SourceOver (aka Normal) is a blending tool which blends together source and target
+// taking into account alpha channel of both plus the additional opacity of the source.
 type SourceOver struct {
 	*Tool
 	opacity int
 }
 
+// BlendSourceToTarget blends source into target. Only position of the target Selection
+// is used and the source is not clamped by the target size.
 func (s *SourceOver) BlendSourceToTargetColor(source, target image.Color) image.Color {
 	return source
 }
 
+// SetOpacity specifies the opacity of source selection.
 func (s *SourceOver) SetOpacity(opacity int) {
 	s.opacity = opacity
 }
 
+// Tool is a customizable blending tool which blends together two selections. It uses
+// ColorBlender implementation for actual blending of two pixel colors.
 type Tool struct {
 	colorBlender ColorBlender
 }
