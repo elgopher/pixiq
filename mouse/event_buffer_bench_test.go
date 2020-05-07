72d799c5c5017e86ba64083281d744f1208e5a69
Jacek Olszak <jacekolszak@gmail.com>
2020-05-07 19:44:57 +0200

    Invoke more methods in event buffer bench test

--- mouse/event_buffer_bench_test.go
+++ mouse/event_buffer_bench_test.go
@@ -14,7 +14,10 @@ func BenchmarkMouseEvents(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		for j := 0; j < size*2; j++ {
+			events.Add(mouse.NewReleasedEvent(mouse.Left))
 			events.Add(mouse.NewPressedEvent(mouse.Left))
+			events.Add(mouse.NewScrolledEvent(1, 2))
+			events.Add(mouse.NewMovedEvent(1, 2, 1, 2, true))
 		}
 		for {
 			_, ok := events.Poll()
