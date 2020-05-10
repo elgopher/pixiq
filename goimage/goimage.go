90ffc39ed26b5e2d8b0b597e7333db13d687be30
Jacek Olszak <jacekolszak@gmail.com>
2020-05-10 15:25:42 +0200

    Reuse code

--- goimage/goimage.go
+++ goimage/goimage.go
@@ -5,12 +5,29 @@ package goimage
 import (
 	stdimage "image"
 	"image/color"
+	"image/draw"
 
 	"github.com/jacekolszak/pixiq/image"
 )
 
 // FromSelection creates a standard Go Image from Pixiq image.Selection
 func FromSelection(source image.Selection, options ...Option) stdimage.Image {
+	opts := buildOpts(source, options...)
+	if opts.height == 0 {
+		return stdimage.NewRGBA(stdimage.Rectangle{})
+	}
+	bounds := stdimage.Rect(0, 0, opts.width*opts.zoom, opts.height*opts.zoom)
+	target := stdimage.NewRGBA(bounds)
+	FillWithSelection(target, source, options...)
+	return target
+}
+
+type opts struct {
+	zoom          int
+	width, height int
+}
+
+func buildOpts(source image.Selection, options ...Option) opts {
 	opts := opts{
 		zoom: 1,
 	}
@@ -18,32 +35,28 @@ func FromSelection(source image.Selection, options ...Option) stdimage.Image {
 		opts = option(opts)
 	}
 	lines := source.Lines()
-	if lines.Length() == 0 {
-		return stdimage.NewRGBA(stdimage.Rectangle{})
+	opts.height = lines.Length()
+	if opts.height != 0 {
+		opts.width = len(lines.LineForRead(0))
 	}
-	width := len(lines.LineForRead(0))
-	height := lines.Length()
-	img := stdimage.NewRGBA(stdimage.Rect(0, 0, width*opts.zoom, height*opts.zoom))
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
+	return opts
+}
+
+func FillWithSelection(target draw.Image, source image.Selection, options ...Option) {
+	opts := buildOpts(source, options...)
+	lines := source.Lines()
+	for y := 0; y < opts.height; y++ {
+		for x := 0; x < opts.width; x++ {
 			line := lines.LineForRead(y)
 			c := line[x]
 			rgba := color.RGBA{R: c.R(), G: c.G(), B: c.B(), A: c.A()}
 			for zy := 0; zy < opts.zoom; zy++ {
 				for zx := 0; zx < opts.zoom; zx++ {
-					img.Set(x*opts.zoom+zx, y*opts.zoom+zy, rgba)
+					target.Set(x*opts.zoom+zx, y*opts.zoom+zy, rgba)
 				}
 			}
 		}
 	}
-	return img
-}
-
-type opts struct {
-	zoom int
-}
-
-func FillWithSelection(target stdimage.Image, source image.Selection, options ...Option) {
 }
 
 type Option func(opts) opts
--- goimage/goimage_test.go
+++ goimage/goimage_test.go
@@ -3,6 +3,7 @@ package goimage_test
 import (
 	stdimage "image"
 	"image/color"
+	"image/draw"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -67,7 +68,7 @@ func TestFromSelection(t *testing.T) {
 		for name, test := range tests {
 			t.Run(name, func(t *testing.T) {
 				// when
-				stdImg := goimage.FromSelection(test.source)
+				stdImg := goimage.FromSelection(test.source, test.opts...)
 				// then
 				expectedBounds := stdimage.Rectangle{
 					Max: stdimage.Point{
@@ -160,6 +161,110 @@ func TestFromSelection(t *testing.T) {
 				// when
 				stdImg := goimage.FromSelection(test.source, test.opts...)
 				assertPixels(t, stdImg, test.expectedPixels)
+			})
+		}
+	})
+
+}
+
+func TestFillWithSelection(t *testing.T) {
+	t.Run("should fill standard Go image with pixels from Selection", func(t *testing.T) {
+		img := image.New(fake.NewAcceleratedImage(2, 2))
+		selection := img.WholeImageSelection()
+		pix00 := color.RGBA{R: 0, G: 10, B: 20, A: 30}
+		selection.SetColor(0, 0, image.RGBA(0, 10, 20, 30))
+		pix10 := color.RGBA{R: 100, G: 110, B: 120, A: 130}
+		selection.SetColor(1, 0, image.RGBA(100, 110, 120, 130))
+		pix01 := color.RGBA{R: 150, G: 160, B: 170, A: 180}
+		selection.SetColor(0, 1, image.RGBA(150, 160, 170, 180))
+		pix11 := color.RGBA{R: 200, G: 210, B: 220, A: 230}
+		selection.SetColor(1, 1, image.RGBA(200, 210, 220, 230))
+		tests := map[string]struct {
+			target         draw.Image
+			source         image.Selection
+			opts           []goimage.Option
+			expectedPixels [][]color.RGBA
+		}{
+			"top-left": {
+				target: stdimage.NewRGBA(stdimage.Rect(0, 0, 1, 1)),
+				source: img.Selection(0, 0).WithSize(1, 1),
+				expectedPixels: [][]color.RGBA{
+					{
+						pix00,
+					},
+				},
+			},
+			"bottom-right": {
+				target: stdimage.NewRGBA(stdimage.Rect(0, 0, 1, 1)),
+				source: img.Selection(1, 1).WithSize(1, 1),
+				expectedPixels: [][]color.RGBA{
+					{
+						pix11,
+					},
+				},
+			},
+			"top": {
+				target: stdimage.NewRGBA(stdimage.Rect(0, 0, 2, 1)),
+				source: img.Selection(0, 0).WithSize(2, 1),
+				expectedPixels: [][]color.RGBA{
+					{
+						pix00, pix10,
+					},
+				},
+			},
+			"left": {
+				target: stdimage.NewRGBA(stdimage.Rect(0, 0, 1, 2)),
+				source: img.Selection(0, 0).WithSize(1, 2),
+				expectedPixels: [][]color.RGBA{
+					{
+						pix00,
+					},
+					{
+						pix01,
+					},
+				},
+			},
+			"whole": {
+				target: stdimage.NewRGBA(stdimage.Rect(0, 0, 2, 2)),
+				source: img.WholeImageSelection(),
+				expectedPixels: [][]color.RGBA{
+					{
+						pix00, pix10,
+					},
+					{
+						pix01, pix11,
+					},
+				},
+			},
+			"zoom top-left": {
+				target: stdimage.NewRGBA(stdimage.Rect(0, 0, 2, 2)),
+				source: img.Selection(0, 0).WithSize(1, 1),
+				opts:   []goimage.Option{goimage.Zoom(2)},
+				expectedPixels: [][]color.RGBA{
+					{
+						pix00, pix00,
+					},
+					{
+						pix00, pix00,
+					},
+				},
+			},
+			"image smaller than selection": {
+				target: stdimage.NewRGBA(stdimage.Rect(0, 0, 1, 1)),
+				source: img.WholeImageSelection(),
+				expectedPixels: [][]color.RGBA{
+					{
+						pix00,
+					},
+				},
+			},
+		}
+		for name, test := range tests {
+			t.Run(name, func(t *testing.T) {
+				targetImage := test.target
+				// when
+				goimage.FillWithSelection(targetImage, test.source, test.opts...)
+				assertPixels(t, targetImage, test.expectedPixels)
 			})
 		}
 	})
