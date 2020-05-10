e0e20e9ce221674c87d6040d6fd86c65723bd2ca
Jacek Olszak <jacekolszak@gmail.com>
2020-05-10 10:14:20 +0200

    Rename methods

--- examples/goimage/main.go
+++ examples/goimage/main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"image"
+
 	"github.com/jacekolszak/pixiq/glfw"
 	"github.com/jacekolszak/pixiq/goimage"
-	"image"
 )
 
 func main() {
 	glfw.RunOrDie(func(openGL *glfw.OpenGL) {
 		pixiqImage := openGL.NewImage(4, 4)
 		selection := pixiqImage.WholeImageSelection()
-		//
+		// Create a new standard Go image.Image from Pixiq Selection
 		newImage := goimage.FromSelection(selection, goimage.Zoom(3))
-		//
+		// Or fill existing standard Go image.Image
 		existingImage := image.NewRGBA(image.Rect(0, 0, selection.Width(), selection.Height()))
-		goimage.CopyFromSelection(selection, existingImage)
-		//
+		goimage.FillWithSelection(existingImage, selection)
+		// Copy standard Go image.Image to Selection
 		goimage.CopyToSelection(newImage, selection)
 	})
 }
--- goimage/goimage.go
+++ goimage/goimage.go
@@ -1,17 +1,19 @@
-// Package goimage provides functions for converting Pixiq's image.Image into standard Go image.Image and vice-versa
+// Package goimage provides functions for converting Pixiq image.Selection
+// into standard Go image.Image and vice-versa
 package goimage
 
 import (
-	"github.com/jacekolszak/pixiq/image"
 	stdimage "image"
+
+	"github.com/jacekolszak/pixiq/image"
 )
 
-// FromSelection creates a standard Go Image from Pixiq's image.Selection
+// FromSelection creates a standard Go Image from Pixiq image.Selection
 func FromSelection(source image.Selection, options ...Option) stdimage.Image {
 	return stdimage.NewRGBA(stdimage.Rect(0, 0, source.Width(), source.Height()))
 }
 
-func CopyFromSelection(source image.Selection, target stdimage.Image) {
+func FillWithSelection(target stdimage.Image, source image.Selection) {
 }
 
 type Option func()
@@ -22,7 +24,7 @@ func Zoom(zoom int) Option {
 	}
 }
 
-// CopyToSelection copies standard Go Image to Pixiq's image.Selection
+// CopyToSelection copies standard Go Image to Pixiq image.Selection
 func CopyToSelection(source stdimage.Image, target image.Selection) {
 
 }
