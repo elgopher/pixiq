94fef39355ce8ed78620dca35c8128f440fe1cc0
Jacek Olszak <jacekolszak@gmail.com>
2020-05-20 11:36:17 +0200

    Remove use of go-routines in multiple windows example (#120)

--- examples/windows/multiple/main.go
+++ examples/windows/multiple/main.go
@@ -1,69 +1,63 @@
 package main
 
 import (
-	"log"
-	"sync"
-
-	"github.com/jacekolszak/pixiq/colornames"
+	"github.com/jacekolszak/pixiq/decoder"
+	"github.com/jacekolszak/pixiq/glblend"
 	"github.com/jacekolszak/pixiq/glfw"
-	"github.com/jacekolszak/pixiq/image"
-	"github.com/jacekolszak/pixiq/loop"
+	"log"
 )
 
 // This example shows how to open two windows at the same time.
 //
-// Please note that this functionality is experimental and may change in the
+// Please note that this functionality is EXPERIMENTAL and may change in the
 // near future. Such feature may be harmful for overall performance of Pixiq.
+//
+// Please note also that at the moment most Pixiq primitives are not goroutine-safe.
+// You can't safely use one *glfw.OpenGL instance in two different go-routines.
 func main() {
 	glfw.RunOrDie(func(openGL *glfw.OpenGL) {
-		redWindow, err := openGL.OpenWindow(320, 180, glfw.Title("red"))
+		redWindow, err := openGL.OpenWindow(11, 11, glfw.Zoom(20), glfw.Title("red"))
 		if err != nil {
 			log.Panicf("red OpenWindow failed: %v", err)
 		}
-		blueWindow, err := openGL.OpenWindow(250, 90, glfw.Title("blue"))
+		blueWindow, err := openGL.OpenWindow(11, 11, glfw.Zoom(20), glfw.Title("blue"))
 		if err != nil {
 			log.Panicf("blue OpenWindow failed: %v", err)
 		}
 
-		var waitUntilAllClosed sync.WaitGroup
-		waitUntilAllClosed.Add(2)
+		imageDecoder := decoder.New(openGL)
 
-		// Start the loop in the background, because loop.Run blocks
-		// the current goroutine.
-		go func() {
-			loop.Run(redWindow, func(frame *loop.Frame) {
-				fillScreenWith(frame.Screen(), colornames.Red)
-				if redWindow.ShouldClose() {
-					frame.StopLoopEventually()
-				}
-			})
-			// clean resources
-			redWindow.Close()
-			waitUntilAllClosed.Done()
-		}()
-
-		// Start another one.
-		go func() {
-			loop.Run(blueWindow, func(frame *loop.Frame) {
-				fillScreenWith(frame.Screen(), colornames.Blue)
-				if blueWindow.ShouldClose() {
-					frame.StopLoopEventually()
-				}
-			})
-			blueWindow.Close()
-			waitUntilAllClosed.Done()
-		}()
-
-		// wait for all windows to be closed
-		waitUntilAllClosed.Wait()
-	})
-}
-
-// fillScreenWith returns a function filling whole Screen with specific color.
-func fillScreenWith(screen image.Selection, color image.Color) {
-	for y := 0; y < screen.Height(); y++ {
-		for x := 0; x < screen.Width(); x++ {
-			screen.SetColor(x, y, color)
+		redImg, err := imageDecoder.DecodeFile("examples/windows/multiple/red.png")
+		if err != nil {
+			log.Panicf("DecodeFile failed: %v", err)
 		}
-	}
+
+		blueImg, err := imageDecoder.DecodeFile("examples/windows/multiple/blue.png")
+		if err != nil {
+			log.Panicf("DecodeFile failed: %v", err)
+		}
+
+		redScreen := redWindow.Image().WholeImageSelection()
+		blueScreen := blueWindow.Image().WholeImageSelection()
+
+		blender, err := glblend.NewSourceOver(openGL.Context())
+		if err != nil {
+			log.Panicf("glblend.NewSourceOver failed: %v", err)
+		}
+
+		for {
+			// Draw windows sequentially. Drawing it in parallel does not make much sense with
+			// current glfw.OpenGL implementation because on the end all OpenGL calls are executed
+			// in a main thread. glfw.OpenGL is also not goroutines-safe and you can't
+			// run commands (such as those used by glblend package) atomically.
+			blender.BlendSourceToTarget(redImg.WholeImageSelection(), redScreen)
+			redWindow.Draw()
+			blender.BlendSourceToTarget(blueImg.WholeImageSelection(), blueScreen)
+			blueWindow.Draw()
+			if redWindow.ShouldClose() || blueWindow.ShouldClose() {
+				return
+			}
+		}
+
+	})
 }
